8c02b0797cccff844926fa9f5cafbd9603a2d026
Victor Alessander <alessander@protonmail.com>
2017-04-20 14:27:50 +0000

    Implemented authentication on Facebook

--- core/views.py
+++ core/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from .models import Category, Product
+from django.contrib.auth.decorators import login_required
+
 
 class Index(TemplateView):
 	template_name = 'index.html'
 
 
+@login_required
 def category_list(request):
 
 	categories = Category.objects.all()
@@ -13,6 +16,7 @@ def category_list(request):
 	return render(request, 'categories.html', {'categories': categories})
 
 
+@login_required
 def product_list(request, category):
 
 	products = Product.objects.filter(category__name=category)
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('core.urls', namespace='core')),
+	url(r'^oauth/', include('social_django.urls', namespace='social')),
+	url(r'^login/$', auth_views.login, name='login'),
+	url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
 ]
