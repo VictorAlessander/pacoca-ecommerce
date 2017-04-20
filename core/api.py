b2a51de67630859e772a71240e5705a130eb8956
Victor Alessander <alessander@protonmail.com>
2017-04-20 14:26:44 +0000

    Implemented api with tastypie

---
+++ core/api.py
@@ -0,0 +1,19 @@
+from tastypie.resources import ModelResource
+from .models import Category, Product
+from tastypie import fields
+
+
+class CategoryResource(ModelResource):
+
+	class Meta:
+		queryset = Category.objects.all()
+		resource_name = 'category'
+
+
+class ProductResource(ModelResource):
+	category = fields.CharField(attribute="category")
+
+	class Meta:
+		queryset = Product.objects.all()
+		resource_name = 'product'
+		
--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,18 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
+from .api import CategoryResource, ProductResource
+
+
+category_resource = CategoryResource()
+product_resource = ProductResource()
 
 
 urlpatterns = [
-	url(r'^$', views.Index.as_view()),
+	url(r'^$', views.Index.as_view(), name='index'),
 	url(r'^categories/', views.category_list, name='category_list'),
 	url(r'^products/(?P<category>\w+)/', views.product_list, name='product_list'),
+
+
+	url(r'^api/', include(category_resource.urls)),
+	url(r'^api/', include(product_resource.urls)),
 ]
