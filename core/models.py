06c085f61282744c0182cc4fe269be2cff15c9ae
Victor Alessander <alessander@protonmail.com>
2017-04-22 01:41:52 +0000

    Created class 'Cart'

--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Category(models.Model):
@@ -19,3 +20,17 @@ class Product(models.Model):
 
 	def __str__(self):
 		return self.name
+
+
+class Cart(models.Model):
+
+	cod = models.PositiveIntegerField()
+	name = models.CharField(max_length=150)
+	price = models.DecimalField(decimal_places=2, max_digits=8)
+	quantity = models.PositiveIntegerField(default=1)
+	date = models.DateTimeField(null=True, blank=True)
+
+	def save_order(self):
+		owner = models.ForeignKey('auth.User')
+		self.date = timezone.now()
+		self.save()
