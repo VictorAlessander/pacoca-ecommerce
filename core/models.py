463ebdfc543ceefbab818da356ed210d66e0caa5
Victor Alessander <alessander@protonmail.com>
2017-04-24 14:37:35 +0000

    Removed field 'description' on model 'Category' and some little changes

--- core/models.py
+++ core/models.py
@@ -7,7 +7,6 @@ class Category(models.Model):
 
 	cod = models.PositiveIntegerField()
 	name = models.CharField(max_length=30)
-	description = models.TextField()
 
 	def __str__(self):
 		return self.name
