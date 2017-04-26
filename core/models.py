d3124ddfe071a7aa6a11f53d6cb72a61c5fdc7d0
Victor Alessander <alessander@protonmail.com>
2017-04-26 16:00:15 -0300

    Changed name of field and created function 'total'

--- core/models.py
+++ core/models.py
@@ -50,11 +50,17 @@ class Order(models.Model):
 	quantity = models.PositiveIntegerField(default=1)
 	date = models.DateTimeField(blank=True, null=True)
 	owner = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='Usuário')
-	session_id = models.CharField(max_length=40)
+	order_id = models.CharField(max_length=40)
 
 	def save_order(self):
 		self.date = timezone.now()
 		self.save()
 
-	def __str__(self):
-		return 'Order #{}'.format(self.date)
+	def total(self, session, user):
+		aggregate_queryset = Order.objects.filter(order_id=session, owner=user).aggregate(
+			total_price=Sum(models.F('price') * models.F('quantity'),
+				output_field=models.DecimalField(decimal_places=2, max_digits=8)
+				)
+			)
+
+		return aggregate_queryset['total_price']
