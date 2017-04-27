6158737bc6d59f2da3598a254a7fe8d3ee3df4fd
Victor Alessander <alessander@protonmail.com>
2017-04-26 21:20:56 -0300

    Little implementations

--- core/static/javascript/order.js
+++ core/static/javascript/order.js
@@ -1,6 +1,6 @@
 function finish_order(){
 	if(confirm("Finish the order?")){
-		return alert("Finished");
+		return alert("Your order has been successfully completed.");
 	}
 	else{
 		return false;
